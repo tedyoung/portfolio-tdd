e654f2bc83a1e18ddaca4aacdec116f1db116474
Ted M. Young <tedyoung@gmail.com>
2020-09-02 20:35:55 -0700

    Cleanup refactoring.

--- src/main/java/com/tedmyoung/portfolio/Portfolio.java
+++ src/main/java/com/tedmyoung/portfolio/Portfolio.java
@@ -6,34 +6,43 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Portfolio {
-  private int balance = 0;
   private final List<Holding> holdings = new ArrayList<>();
   private Holding cashHolding = new Holding(0, "CASH", 1, LocalDate.now());
 
   public int value() {
-    return balance;
+    return allStockHoldingsPlusCash().stream()
+                                     .mapToInt(Holding::value)
+                                     .sum();
   }
 
   public void deposit(int amount) {
+    updateCashHoldingTo(amount);
+  }
+
+  private void updateCashHoldingTo(int amount) {
     cashHolding = new Holding(amount, "CASH", 1, LocalDate.now());
-    balance += cashHolding.value();
   }
 
   public void buy(int shares, String symbol, int price, LocalDate dateOfPurchase) {
+    addHolding(shares, symbol, price, dateOfPurchase);
+    updateCashHoldingTo(cashHolding.value() - shares * price);
+  }
+
+  private void addHolding(int shares, String symbol, int price, LocalDate dateOfPurchase) {
     Holding holding = new Holding(shares, symbol, price, dateOfPurchase);
     holdings.add(holding);
-
-    int cashValue = cashHolding.value();
-    cashHolding = new Holding(cashValue - holding.value(), "CASH", 1, LocalDate.now());
   }
 
   public List<String> holdings() {
-    List<Holding> displayList = new ArrayList<>();
+    return allStockHoldingsPlusCash().stream()
+                                     .map(Holding::holdingString)
+                                     .collect(Collectors.toUnmodifiableList());
+  }
+
+  private List<Holding> allStockHoldingsPlusCash() {
+    List<Holding> displayList = new ArrayList<>(holdings);
     displayList.add(cashHolding);
-    displayList.addAll(holdings);
-    return displayList.stream()
-                      .map(Holding::holdingString)
-                      .collect(Collectors.toUnmodifiableList());
+    return displayList;
   }
 
   public int cash() {
